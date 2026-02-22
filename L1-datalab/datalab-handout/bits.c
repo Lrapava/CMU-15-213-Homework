bfeeeac55d67319bb63acf47342637c6c4a27689
Luka Rapava <rapava04@gmail.com>
2026-02-22 15:02:23 +0400

    L1 - First 9 puzzles (20/36)

--- L1-datalab/datalab-handout/bits.c
+++ L1-datalab/datalab-handout/bits.c
@@ -143,7 +143,7 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) {
-  return 2;
+  return (~(x&y))&(~((~x)&(~y)));
 }
 /* 
  * tmin - return minimum two's complement integer 
@@ -152,9 +152,8 @@ int bitXor(int x, int y) {
  *   Rating: 1
  */
 int tmin(void) {
-
-  return 2;
-
+  // for some reason this violates no rules, but I don't like it
+  return 1<<31;
 }
 //2
 /*
@@ -165,7 +164,7 @@ int tmin(void) {
  *   Rating: 1
  */
 int isTmax(int x) {
-  return 2;
+	return (!!(x^(~0))) & !((x+1+x)^(~0));
 }
 /* 
  * allOddBits - return 1 if all odd-numbered bits in word set to 1
@@ -176,7 +175,7 @@ int isTmax(int x) {
  *   Rating: 2
  */
 int allOddBits(int x) {
-  return 2;
+  return !((x & (x >> 8) & (x >> 16) & (x >> 24) & 170) ^ 170) ;
 }
 /* 
  * negate - return -x 
@@ -186,7 +185,7 @@ int allOddBits(int x) {
  *   Rating: 2
  */
 int negate(int x) {
-  return 2;
+  return ~x+1;
 }
 //3
 /* 
@@ -199,7 +198,7 @@ int negate(int x) {
  *   Rating: 3
  */
 int isAsciiDigit(int x) {
-  return 2;
+  return (!(x&~63)) & ((!(x&8)) | (!(x&6))) & !(48&~x);
 }
 /* 
  * conditional - same as x ? y : z 
@@ -209,7 +208,8 @@ int isAsciiDigit(int x) {
  *   Rating: 3
  */
 int conditional(int x, int y, int z) {
-  return 2;
+  int m = ((!!x) + ~0);
+  return (m&z) | ((~m)&y);
 }
 /* 
  * isLessOrEqual - if x <= y  then return 1, else return 0 
@@ -219,7 +219,12 @@ int conditional(int x, int y, int z) {
  *   Rating: 3
  */
 int isLessOrEqual(int x, int y) {
-  return 2;
+  int neg_x = (x >> 31) & 1;
+  int neg_y = (y >> 31) & 1;
+  int same_sign = !(neg_x^neg_y);
+  int diff = y + ~x + 1;
+  int neg_diff = (diff >> 31) & 1;
+  return (neg_x & !neg_y) | (same_sign & !neg_diff);
 }
 //4
 /* 
@@ -231,7 +236,13 @@ int isLessOrEqual(int x, int y) {
  *   Rating: 4 
  */
 int logicalNeg(int x) {
-  return 2;
+  int y;
+  y = x | (x>>16);
+  x = y | (y>>8);
+  y = x | (x>>4);
+  x = y | (y>>2);
+  y = x | (x>>1);
+  return (y & 1) ^ 1;
 }
 /* howManyBits - return the minimum number of bits required to represent x in
  *             two's complement
@@ -246,7 +257,7 @@ int logicalNeg(int x) {
  *  Rating: 4
  */
 int howManyBits(int x) {
-  return 0;
+	return 0;
 }
 //float
 /* 
